aaad7e04de279c3c7bdd99e54122419dc462f07a
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-08-20 21:59:33 -0300

    refactor: refatorando código html semantico e recriando o component

---
+++ src/components/ExerciseList.tsx
@@ -0,0 +1,22 @@
+export function ExerciseList() {
+    return (
+        <>
+            <main>
+                <h2>Exercicio 1:</h2>
+                <h3>Agachamento</h3>
+                <p>Como fazer: colocar os pés afastados na largura do quadril e manter as costas retas e o abdômen contraído. Agachar, até as coxas ficarem paralelas ao chão, inspirando, e levantar, soltando o ar.</p>
+                <p>5x12</p>
+
+                <h2>Exercicio 2:</h2>
+                <h3>Stiff</h3>
+                <p>Como fazer: de pé, afastar um pouco os pés na largura dos quadris, manter a coluna reta, o abdômen contraído e os ombros levemente para trás. Manter os braços retos posicionados na frente do corpo, próximo as coxas.</p>
+                <p>4x15</p>
+
+                <h2>Exercicio 3:</h2>
+                <h3>Elevação pélvica</h3>
+                <p>Como fazer: para fazer a elevação pélvica no chão, deve-se deitar num tapete, com a barriga para cima e apoiar os dois pés no chão, afastados na largura do quadril.</p>
+                <p>3x15</p>
+            </main>
+        </>
+    )
+}
