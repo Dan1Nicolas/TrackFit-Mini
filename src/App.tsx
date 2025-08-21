95f915f017ff0ecd6ae8e810e39dc06bed5dd122
Dan1Nicolas <kanashiitami66@gmail.com>
2025-08-20 22:36:29 -0300

    feat: Criando componente MotivationalMessage com props e estilizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { MotivationalMessage } from "./components/MotivationalMessage"
 function App() {
   return (
     <>
+      <footer style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: "20px",
+          backgroundColor: "grey",
+          padding: "20px"
+        }}>
+
+        <MotivationalMessage
+        message="Seja natural !"
+        author="Zeka Ramos"
+      />
+
+      <MotivationalMessage
+        message="O corpo alcança o que a mente acredita !"
+        author="Internet"
+      />
       
+      <MotivationalMessage
+        message="No pain, no gain !"
+        author="Intenet"
+      />
+      </footer>
     </>
   )
 }
