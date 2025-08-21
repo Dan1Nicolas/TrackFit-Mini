d919e91f404cb5a2d8b6895683475af8cfd57390
CarolinaaFontes <acfonts1@gmail.com>
2025-08-20 22:00:26 -0300

    feat: elementos do header

---
+++ src/components/welcome.tsx
@@ -0,0 +1,16 @@
+function Welcome() {
+  return (
+    <>
+     <h1>TrackFit</h1>
+     <h2>Seja bem-vindo</h2>
+
+     <p>
+        "Acredite no seu potencial e avance com confiança, pois cada passo é uma conquista &#x1F600;"
+    </p>
+    
+    <p> &#x1F600;</p>
+    </>
+  )
+}
+
+export default Welcome
